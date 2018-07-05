daddb7f15238732b102b88845b43724a4ece9942
codebymatt <mattcraig365@gmail.com>
2018-07-05 20:10:48 +0100

    Clean up index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,22 +11,8 @@ var slowReveal = ScrollReveal({
 quickReveal.reveal('.spread-image');
 slowReveal.reveal('.fade-in');
 
-var music   = document.getElementById("music-wrapper");
-var shows   = document.getElementById("shows-wrapper");
-var about   = document.getElementById("about-wrapper");
-var contact = document.getElementById("contact-wrapper");
-
-// If you're reading this code, please dont't hate.
-// Yes I'd like it to be DRYer, but I'd also like to
-// ship this product, and making it modular isn't going
-// to save that much time in the future
-
-function scrollToMusic()  { scrollToDiv(music); }
-function scrollToShows()  { scrollToDiv(shows); }
-function scrollToAbout()  { scrollToDiv(about); }
-function scrollToContact()  { scrollToDiv(contact); }
-
 function scrollToDiv(div) {
-// Spelling 'behavior' with a 'u' breaks smooth scrolling..
-    div.scrollIntoView({behavior: "smooth", block: "start"});
+    el = document.getElementById(div)
+    // Spelling 'behavior' with a 'u' breaks smooth scrolling..
+    el.scrollIntoView({behavior: "smooth", block: "start"});
 }
